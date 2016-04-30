17e0e7660346d99b505ae770da72f7b3115e79e2
Amahzoune Ranya <Amahzoune Ranya@AmahzouneRanya>
2016-04-30 17:37:02 +0200

    main2

--- Projet_Graphes_Amahzoune/src/Main.java
+++ Projet_Graphes_Amahzoune/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Helloo world");
+		System.out.println("Helloo world 1");
 
 	}
 
